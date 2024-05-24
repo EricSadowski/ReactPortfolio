6237cb27297f58095d36fe6811a358b6c27adf97
EricSadowski <118220751+EricSadowski@users.noreply.github.com>
2024-05-24 18:44:07 -0400

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -10,10 +10,7 @@ const About = () => {
            <div className="a-right">
            <h1 className="a-title">About Me</h1>
         <p className="a-desc">
-        Eric Sadowski is a Web Developer and Multi-Media Artist from Montréal, 
-        Québec. Born in Vancouver, British Columbia, he has been using computers 
-        since he could talk and he can fondly remember the sound of dial up.
-         His skills include: HTML/CSS, Photoshop, Javascript, C#, ASP.NET, React, SpringBoot, Node.js, Java, PHP and MySQL. 
+        Eric Sadowski is a skilled Web Developer and Multi-Media Artist from Montréal, Québec. Born in Vancouver, British Columbia, he has a strong background in HTML/CSS, Photoshop, Javascript, C#, ASP.NET, React, SpringBoot, Node.js, Java, PHP, and MySQL. With a passion for technology and art, Eric brings a unique blend of creativity and technical expertise to his work.
         </p>
            </div>
         </div>
